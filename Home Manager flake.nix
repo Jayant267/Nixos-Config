b5032e5284851be83ce69b0d7aa639c48993bac0
Jayant Jagtap <jayantjagtap.mils@gmail.com>
2025-07-30 12:41:17 +0530

    Flake System

---
+++ Home Manager flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "One flake to rule them all";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } :
+  {
+      nixosConfigurations = {
+        niki = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+
+      homeConfigurations = {
+        y = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          modules = [ ./home.nix ];
+        };
+      };
+  };
+
+}
